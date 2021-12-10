94fcf798c381ff12e1478a22b6307f64025c8542
Nuccino92 <anthnucci@hotmail.com>
2021-12-10 03:01:08 -0500

    set up app browser routes with a homepage path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LoggedInPackage from "./components/logged-in/LoggedInPackage";
+import LoggedOutPackage from "./components/logged-out/LoggedOutPackage";
+import Homepage from "./components/homepage/Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <LoggedInPackage />
+        {/* <LoggedOutPackage /> */}
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
