98e7768e7af8e2aff75098c7a49a85b5d0102e91
Nuccino92 <anthnucci@hotmail.com>
2021-12-10 03:03:14 -0500

    added container for styling with a link to the homepage

--- src/components/logged-in/header/LoggedInHeader.js
+++ src/components/logged-in/header/LoggedInHeader.js
@@ -6,32 +6,46 @@ import {
 import { TiMicrophone } from "react-icons/ti";
 import { BsGrid3X3Gap } from "react-icons/bs";
 import { IoIosNotificationsOutline } from "react-icons/io";
+import { Link } from "react-router-dom";
 import youtubeIcon from "/home/anthony/the_odin_project/youtube-mock/src/components/header/youtube-icon.png";
 
 const LoggedInHeader = () => {
   return (
     <div className="header">
       <div className="left-header-container">
-        <AiOutlineMenu className="header-menu-icon" />
+        <AiOutlineMenu size={20} className="header-menu-icon" />
         <div className="youtube-menu-icon-container">
-          <img
-            className="youtube-menu-icon"
-            src={youtubeIcon}
-            alt="youtube-icon"
-          />
+          <Link to="/">
+            <img
+              placeholder="Search"
+              title="YouTube Home"
+              className="youtube-menu-icon"
+              src={youtubeIcon}
+              alt="youtube-icon"
+            />
+          </Link>
         </div>
       </div>
       <div className="search-bar-container">
-        <label for="search-bar"></label>
-        <input type="text" name="search-bar" className="search-bar"></input>
-        <AiOutlineSearch className="search-icon" />
+        <div className="search-bar-border">
+          <label htmlFor="search-bar"></label>
+          <input
+            placeholder="Search"
+            type="text"
+            name="search-bar"
+            className="search-bar"
+          ></input>
+          <div className="search-icon-container" title="search">
+            <AiOutlineSearch size={28} className="search-icon" />
+          </div>
+        </div>
+        <TiMicrophone size={22} className="microphone-icon" />
       </div>
-      <TiMicrophone className="microphone-icon" />
       <div className="right-header-container">
-        <AiOutlineVideoCameraAdd />
-        <BsGrid3X3Gap />
-        <IoIosNotificationsOutline />
-        <div>Picture</div>
+        <AiOutlineVideoCameraAdd title="Create" size={23} />
+        <BsGrid3X3Gap title="YouTube Apps" size={20} />
+        <IoIosNotificationsOutline title="Notifications" size={27} />
+        <div className="profile-picture">Picture</div>
       </div>
     </div>
   );
