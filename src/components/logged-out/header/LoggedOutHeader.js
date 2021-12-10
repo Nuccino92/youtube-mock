ed7ea2d3bcb7c24c6a5c290c872da03f392b3052
Nuccino92 <anthnucci@hotmail.com>
2021-12-10 03:05:53 -0500

    added divs for styling/ resized react icons

--- src/components/logged-out/header/LoggedOutHeader.js
+++ src/components/logged-out/header/LoggedOutHeader.js
@@ -1,32 +1,45 @@
 import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import { TiMicrophone } from "react-icons/ti";
 import { BsGrid3X3Gap, BsThreeDotsVertical } from "react-icons/bs";
-
+import { Link } from "react-router-dom";
 import youtubeIcon from "/home/anthony/the_odin_project/youtube-mock/src/components/header/youtube-icon.png";
 
 const LoggedOutHeader = () => {
   return (
     <div className="header">
       <div className="left-header-container">
-        <AiOutlineMenu className="header-menu-icon" />
+        <AiOutlineMenu size={20} className="header-menu-icon" />
         <div className="youtube-menu-icon-container">
-          <img
-            className="youtube-menu-icon"
-            src={youtubeIcon}
-            alt="youtube-icon"
-          />
+          <Link to="/">
+            <img
+              title="YouTube Home"
+              className="youtube-menu-icon"
+              src={youtubeIcon}
+              alt="youtube-icon"
+            />
+          </Link>
         </div>
       </div>
       <div className="search-bar-container">
-        <label for="search-bar"></label>
-        <input type="text" name="search-bar" className="search-bar"></input>
-        <AiOutlineSearch className="search-icon" />
+        <div className="search-bar-border">
+          <label htmlFor="search-bar"></label>
+          <input
+            placeholder="Search"
+            type="text"
+            name="search-bar"
+            className="search-bar"
+          ></input>
+          <div className="search-icon-container" title="search">
+            <AiOutlineSearch size={28} className="search-icon" />
+          </div>
+        </div>
+        <TiMicrophone size={22} className="microphone-icon" />
       </div>
-      <TiMicrophone className="microphone-icon" />
+
       <div className="right-header-container">
-        <BsGrid3X3Gap />
-        <BsThreeDotsVertical />
-        <button>SIGN IN</button>
+        <BsGrid3X3Gap size={20} title="YouTube Apps" />
+        <BsThreeDotsVertical size={20} title="Settings" />
+        <button className="sign-in-header-btn">SIGN IN</button>
       </div>
     </div>
   );
