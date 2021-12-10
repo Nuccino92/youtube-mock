327e156df6e4275f1547685b696814ae947caa41
Nuccino92 <anthnucci@hotmail.com>
2021-12-10 03:06:50 -0500

    same as logged-in sidebar but w/ some proper changes

--- src/components/logged-out/sidebar/LoggedOutSidebar.js
+++ src/components/logged-out/sidebar/LoggedOutSidebar.js
@@ -1,4 +1,42 @@
+import { MdHome } from "react-icons/md";
+import { FaRegCompass } from "react-icons/fa";
+import { BsCollectionPlay } from "react-icons/bs";
+import { MdOutlineVideoLibrary } from "react-icons/md";
+import { VscHistory } from "react-icons/vsc";
+import { Link } from "react-router-dom";
+
 const LoggedOutSidebar = () => {
-  return <div>this is the sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div>
+        <div className="first-sidebar-container">
+          <Link to="/">
+            <div className="sidebar-inner">
+              <MdHome size={22} />
+              <span>Home</span>
+            </div>
+          </Link>
+          <div className="sidebar-inner">
+            <FaRegCompass size={22} />
+            <span>Explore</span>
+          </div>
+          <div className="sidebar-inner">
+            <BsCollectionPlay size={22} />
+            <span>Subscriptions</span>
+          </div>
+        </div>
+        <div className="second-sidebar-container">
+          <div className="sidebar-inner">
+            <MdOutlineVideoLibrary size={22} />
+            <span>Library</span>
+          </div>
+          <div className="sidebar-inner">
+            <VscHistory size={22} />
+            <span>History</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default LoggedOutSidebar;
