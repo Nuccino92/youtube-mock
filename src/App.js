54c1bbcc733439117a3acd5757e34236f53d516a
Nuccino92 <anthnucci@hotmail.com>
2021-12-11 06:04:59 -0500

    conditionally render logged in or out package w/ context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import React, { useContext } from "react";
+import { AuthContext } from "./context/authContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoggedInPackage from "./components/logged-in/LoggedInPackage";
 import LoggedOutPackage from "./components/logged-out/LoggedOutPackage";
 import Homepage from "./components/homepage/Homepage";
 
 function App() {
+  const { online } = useContext(AuthContext);
+
   return (
     <BrowserRouter>
       <div>
-        <LoggedInPackage />
-        {/* <LoggedOutPackage /> */}
+        {online ? <LoggedInPackage /> : <LoggedOutPackage />}
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
