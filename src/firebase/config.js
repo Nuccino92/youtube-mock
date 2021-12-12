9c439ec89772fcf7ada1b4c64331f8f4096f5fd9
Nuccino92 <anthnucci@hotmail.com>
2021-12-12 01:32:55 -0500

    firebase config setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBw_xVZiiWDP3q21KveCguZX3I5IfN2pLA",
+  authDomain: "mock-e69d2.firebaseapp.com",
+  projectId: "mock-e69d2",
+  storageBucket: "mock-e69d2.appspot.com",
+  messagingSenderId: "1012847646337",
+  appId: "1:1012847646337:web:ea0420d46c52f96ca15f80",
+};
+
+export const app = initializeApp(firebaseConfig);
