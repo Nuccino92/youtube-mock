a71f3f5c46382d040900cc9315e4becbbba443df
Nuccino92 <anthnucci@hotmail.com>
2021-12-12 01:36:15 -0500

    added state which controls sidebar render, imported window size hook to adjust state

--- src/components/logged-in/LoggedInPackage.js
+++ src/components/logged-in/LoggedInPackage.js
@@ -1,13 +1,26 @@
+import { useState, useEffect } from "react";
 import LoggedInHeader from "./header/LoggedInHeader";
 import LoggedInSidebar from "./sidebar/LoggedInSidebar";
 import LoggedInSidebarSmall from "./sidebar/LoggedInSidebarSmall";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const LoggedInPackage = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const size = useWindowSize();
+
+  const handleSidebar = () => {
+    setToggleSidebar(!toggleSidebar);
+  };
+
+  useEffect(() => {
+    size.width < 1331 && setToggleSidebar(true);
+    size.width > 1331 && setToggleSidebar(false);
+  }, [size]);
+
   return (
     <div>
-      <LoggedInHeader />
-      {/* <LoggedInSidebar /> */}
-      <LoggedInSidebarSmall />
+      <LoggedInHeader handleSidebar={handleSidebar} />
+      {toggleSidebar ? <LoggedInSidebarSmall /> : <LoggedInSidebar />}
     </div>
   );
 };
