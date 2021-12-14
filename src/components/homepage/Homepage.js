2a33fb00198781a4d1e20eedcb6272b46ca13398
Nuccino92 <anthnucci@hotmail.com>
2021-12-13 21:35:03 -0500

    added homepage that maps over fetched videos

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,4 +1,15 @@
+import VideoCard from "../VideoCard";
+import { getPopularVideos, getPopVids } from "../../api/getVideos";
+
 const Homepage = () => {
-  return <div>this is the homepage</div>;
+  const { items } = getPopVids.data;
+
+  return (
+    <div className="homepage">
+      {items.map((item, index) => {
+        return <VideoCard info={item} key={index} />;
+      })}
+    </div>
+  );
 };
 export default Homepage;
