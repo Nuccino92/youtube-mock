459940815bb76b08370fe5918566b3627aa1f847
Nuccino92 <anthnucci@hotmail.com>
2021-12-20 19:17:00 -0500

    added loading state

--- src/components/watchScreen/watch-screen-video-list/WatchScreenVideoList.js
+++ src/components/watchScreen/watch-screen-video-list/WatchScreenVideoList.js
@@ -4,15 +4,21 @@ import WatchScreenVideoCard from "./WatchScreenVideoCard";
 
 const WatchScreenVideoList = ({ state }) => {
   const [relatedVideos, setRelatedVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     const callRelatedVideos = async () => {
       setRelatedVideos(await getRelatedVideos(state.id));
+
+      setLoading(false);
     };
     callRelatedVideos();
   }, [state]);
 
-  return (
+  return loading ? (
+    <div>loading...</div>
+  ) : (
     <div className="watch-screen-video-list">
       {relatedVideos.map((video, index) => {
         return <WatchScreenVideoCard info={video} key={index} />;
