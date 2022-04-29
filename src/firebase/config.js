a988e2bb47f07883a9296973ebc102b9969576de
Nuccino92 <anthnucci@hotmail.com>
2022-04-28 23:34:58 -0400

    cleanup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw_xVZiiWDP3q21KveCguZX3I5IfN2pLA",
-  authDomain: "mock-e69d2.firebaseapp.com",
-  projectId: "mock-e69d2",
-  storageBucket: "mock-e69d2.appspot.com",
-  messagingSenderId: "1012847646337",
-  appId: "1:1012847646337:web:ea0420d46c52f96ca15f80",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROKECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
